aad7ab7dda767951da0fc826553a801875353ada
Garrison Locke <gplocke@gmail.com>
2011-05-26 11:30:17 -0500

    Removed code.  Library is now located at https://github.com/PintLabs/PHP-Brewerydb

--- Bn/Service/Brewerydb/Exception.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Exception class to be thrown upon receiving error from the Brewery DB API
- *
- * @see    http://brewerydb.com/api/documentation
- * @author Garrison Locke - http://broken-notebook.com - @gplocke
- */
-class Bn_Service_Brewerydb_Exception extends Exception
-{}
